29761059d85f3814aeaeb799789f2e3f0b1c845f
Hamzaaladaadgithab <aladaada5105390@gmail.com>
2025-03-23 15:04:45 +0300

    first

---
+++ tourism/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:tourism/screens/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Tourism App',
+      
+      theme:ThemeData(
+        primarySwatch:Colors.blue,
+
+      ),
+
+      home:CategoriesScreen(),
+    );
+  }
+}
+
